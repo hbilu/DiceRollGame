16650f64a3b9f1cf66f2d17e7fd9b29c2d76a34c
hbilu <hacerbilu@gmail.com>
2023-05-10 21:31:58 +0200

    update
    enriching visualization and adding comments

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 """
-Calculate the chance that you will reach 100th step of the Eiffel Tower under following conditions:
+Calculate the chance that you will reach 30th step of the Eiffel Tower under following conditions:
 Throw a die 100 times
 - If it is 1 or 2, you will go one step down.
 - If it is 4 or 5, you will go one step up.
@@ -17,6 +17,7 @@ import matplotlib.pyplot as plt
 
 np.random.seed(111)
 walk = []
+
 for i in range(100000):
 	random_walk = [0]
 	for k in range(100):
@@ -40,7 +41,17 @@ for i in range(100000):
 			step = 0
 		random_walk.append(step)
 	walk.append(random_walk)
+
+# np.array(walk).shape -> (100000, 101) ---- walk2.shape -> (101, 100000)
 walk2 = np.transpose(np.array(walk))
+
+# visualizing the first ten game
+plt.plot(walk2[:, 0:10])
+plt.xlim(0, 100)
+plt.xlabel('step walks of the first 10 roll dice game')
+plt.ylabel('stairs of the Eiffel Tower')
+plt.show()
+plt.clf()
 
 # take final step of each 100 dice roll game
 last_steps = walk2[-1, :]
@@ -50,5 +61,6 @@ plt.hist(last_steps, bins=10)
 plt.xlabel('final step reached by each 100 dice roll game')
 plt.show()
 
-# the chance to reach 30th step
-print(np.sum(last_steps >= 30)/100000)
+# the chance (in percentage) to reach 30th step
+print((np.sum(last_steps >= 30)/100000)*100)
+print((np.count_nonzero(last_steps >= 30)/len(last_steps))*100)
